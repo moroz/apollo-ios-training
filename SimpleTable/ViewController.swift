a17190256c703d9ae2ac473a4bdbe9acdff70336
Karol Moroz <k.j.moroz@gmail.com>
2020-05-16 23:22:17 +0800

    Initial Commit

---
+++ SimpleTable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleTable
+//
+//  Created by  Karol Moroz on 2020/5/16.
+//  Copyright © 2020  Karol Moroz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
